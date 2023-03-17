644149e846006b72f5d534608d8876fa0aa9a3ce
vitekkor <viktor.korotkikh@gmail.com>
2023-03-17 23:22:38 +0300

    #5: implement validate fun

--- blockchain/src/main/kotlin/com/vitekkor/blockchain/model/Blockchain.kt
+++ blockchain/src/main/kotlin/com/vitekkor/blockchain/model/Blockchain.kt
@@ -1,13 +1,17 @@
 package com.vitekkor.blockchain.model
 
+import com.vitekkor.blockchain.util.sha256
+
 data class Blockchain(
     val index: Long,
     val previousHash: String,
     val hash: String,
     val data: String,
-    val nonce: String
+    val nonce: Long
 ) {
     fun validate() {
-        TODO("Not implemented")
+        val hashString = "$index$previousHash$data$nonce"
+        val sha256String = hashString.sha256()
+        require(sha256String.takeLast(4).all { it == '0' })
     }
 }
