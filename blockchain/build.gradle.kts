9d11428235538d13b234a30179235823bf9b7acb
vitekkor <viktor.korotkikh@gmail.com>
2023-03-17 22:47:35 +0300

    #1: add base dependencies

--- blockchain/build.gradle.kts
+++ blockchain/build.gradle.kts
@@ -1,10 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
+    id("org.springframework.boot") version "2.7.6"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.spring") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.integration:spring-integration-ip:6.0.3")
+
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
     testImplementation(kotlin("test"))
 }
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
